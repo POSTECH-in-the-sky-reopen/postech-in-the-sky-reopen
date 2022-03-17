3721f391e8aaef17ad2c9ec91bd35611a1dfde74
YangSeungwon <yysw1109@gmail.com>
2022-03-17 14:55:21 +0000

    update item drop rate

--- src/entity/Region.ts
+++ src/entity/Region.ts
@@ -4,15 +4,15 @@ import { Cell } from './Cell'
 export const SUPPLY_APPEARANCE_RATE_PER_LEVEL = [ 10, 10, 10, 10, 10, 10]
 export const MONSTER_STRENGTH_MULTIPLIER_PER_LEVEL = [0, 1.0, 2.0, 3.0, 4.0, 5.0]
 export const MONSTER_ENCHANT_PROBABILITY_PER_LEVEL = [0, 0, 20, 40, 60, 100]
-export const ITEM_DROP_EQUIPABLE_PROBABILITY_PER_LEVEL = [0, 50, 45, 40, 35, 25]
-export const ITEM_DROP_ENCHANT_PROBABILITY_PER_LEVEL = [0, 50, 55, 60, 65, 70]
+export const ITEM_DROP_EQUIPABLE_PROBABILITY_PER_LEVEL = [0, 50, 50, 50, 50, 50]
+export const ITEM_DROP_ENCHANT_PROBABILITY_PER_LEVEL = [0, 50, 50, 50, 50, 50]
 export const ITEM_DROP_LEVEL_PER_LEVEL = [
     [100,  0,  0,  0,  0,  0],
     [  0, 90, 10,  0,  0,  0],
     [  0, 80, 20,  0,  0,  0],
-    [  0, 25, 60, 15,  0,  0],
-    [  0,  0, 45, 50,  5,  0],
-    [  0,  0,  0, 68, 30,  2],
+    [  0, 20, 70, 10,  0,  0],
+    [  0,  0, 48, 50,  3,  0],
+    [  0,  0,  0, 74, 25,  1],
 ]
 export const MONEY_DROP_PER_LEVEL = [ 0, 25, 50, 75, 100, 125]
 
